9f142b63b8a642d6ee3c5b82923e11f8c2929a93
Wim Wauters <info@wapptastic.com>
2014-08-24 18:13:34 +0200

    Fix

--- routes/ip.rb
+++ routes/ip.rb
@@ -5,20 +5,26 @@
   get "/ip" do
     format_response(IP.all, request.accept)
   end
-
-  get "/public" do
-    format_response(request.ip, request.accept)
-  end
-
+  
   get "/ip/:id" do
     ip ||= IP.get(params[:id]) || halt(404)
     format_response(ip, request.accept)
   end
+  
+  get "/ip/:id/public" do
+   ip ||= IP.get(params[:id]) || halt(404)
+    format_response(ip, request.accept)
+  end
 
-  post "/ip/private" do
+  get "/ip/:id/private" do
+    format_response(IP.all, request.accept)
+  end
+
+  post "/ip" do
     body = JSON.parse request.body.read
     ip = IP.create(
       private_ip: body['private'],
+      public_ip: body['public'],
       created_at: Time.now,
       updated_at: Time.now
   )
@@ -26,18 +32,6 @@
   format_response(ip, request.accept)
   end
    
-  post "/ip/public" do
-    body = JSON.parse request.body.read
-    ip = IP.create(
-      public_ip:  body['public'],
-      created_at: Time.now,
-      updated_at: Time.now
-  )
-  status 201
-  format_response(ip, request.accept)
-  end
-
-  
   delete '/ip/:id' do
     ip ||= IP.get(params[:id]) || halt(404)
     halt 500 unless ip.destroy
